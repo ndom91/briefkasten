4c7d44756398282c5e8b25278d78a20d8910af92
ndom91 <yo@ndo.dev>
2022-05-08 20:51:38 +0200

    fix: signin page

--- src/pages/auth/signin.js
+++ src/pages/auth/signin.js
@@ -1,32 +1,34 @@
-import { signIn, getCsrfToken, getProviders } from 'next-auth/react'
-import styles from '../styles/Signin.module.css'
+import { signIn, getProviders } from 'next-auth/react'
+import styles from '../../styles/Signin.module.css'
 
-const Signin = ({ csrfToken, providers }) => {
+const Signin = ({ providers }) => {
   return (
     <div style={{ overflow: 'hidden', position: 'relative' }}>
       <div className={styles.wrapper} />
       <div className={styles.content}>
         <div className={styles.cardWrapper}>
-          <h2>Briefkasten</h2>
           <div className={styles.cardContent}>
-            <input name='csrfToken' type='hidden' defaultValue={csrfToken} />
-            <input placeholder='Email [Example - Please use Github]' size='large' />
-            <button className={styles.primaryBtn}>
-              Submit
-            </button>
-            <hr />
+            <h2 className="mb-4 text-3xl font-black text-[#002140]">
+              Briefkasten
+            </h2>
             {providers &&
-              Object.values(providers).map(provider => (
+              Object.values(providers).map((provider) => (
                 <div key={provider.name} style={{ marginBottom: 0 }}>
-                  <button onClick={() => signIn(provider.id)} >
-                    Sign in with{' '} {provider.name}
+                  <button onClick={() => signIn(provider.id)}>
+                    Sign in with {provider.name}
                   </button>
                 </div>
               ))}
           </div>
         </div>
       </div>
-      <img src='/login_pattern.svg' alt='Pattern Background' layout='fill' className={styles.styledPattern} />
+      {/* eslint-disable-next-line @next/next/no-img-element */}
+      <img
+        src="/login_pattern.svg"
+        alt="Pattern Background"
+        layout="fill"
+        className={styles.styledPattern}
+      />
     </div>
   )
 }
@@ -35,11 +37,9 @@ export default Signin
 
 export async function getServerSideProps(context) {
   const providers = await getProviders()
-  const csrfToken = await getCsrfToken(context)
   return {
     props: {
       providers,
-      csrfToken
     },
   }
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 module.exports = {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    './src/pages/**/*.{js,ts,jsx,tsx}',
+    './src/components/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
     extend: {},
