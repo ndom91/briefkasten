861a471512c2976343fdb8e68220c8a8562fe9c3
ndom91 <yo@ndo.dev>
2022-05-08 20:55:02 +0200

    feat: add @next-auth/prisma-adapter

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,7 +1,10 @@
-import NextAuth from "next-auth"
-import GithubProvider from "next-auth/providers/github"
+import NextAuth from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import prisma from '../../../lib/prisma'
 
 export default NextAuth({
+  adapter: PrismaAdapter(prisma),
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
@@ -9,6 +12,6 @@ export default NextAuth({
     }),
   ],
   pages: {
-    signIn: "/auth/signin",
-  }
+    signIn: '/auth/signin',
+  },
 })
