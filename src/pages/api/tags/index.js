80b873965a4e21b5d29225977958031bb14b5045
ndom91 <yo@ndo.dev>
2022-05-22 01:20:26 +0200

    feat: tags api endpoint

---
+++ src/pages/api/tags/index.js
@@ -0,0 +1,56 @@
+import prisma from '@/lib/prisma'
+import { getSession } from 'next-auth/react'
+
+export default async function handler(req, res) {
+  const session = await getSession({ req })
+  const { method, body } = req
+
+  if (session) {
+    switch (method) {
+      case 'POST': {
+        const { userId, name, emoji } = body
+        if (!name) {
+          return res
+            .status(400)
+            .json({ message: 'Missing required field: name' })
+        }
+
+        const upsertResult = await prisma.tag.insert({
+          data: {
+            name,
+            emoji,
+            userId,
+          },
+        })
+
+        return res.status(200).json(upsertResult)
+      }
+      case 'GET': {
+        return res.status(200).json({ results: ['Hello', 'World'] })
+      }
+      case 'DELETE': {
+        const { id, userId } = body
+
+        if (!id || !userId) {
+          return res.status(400).json({ message: 'Missing required field(s)' })
+        }
+        try {
+          await prisma.tag.delete({
+            where: { id },
+          })
+        } catch (error) {
+          console.error('ERR', error)
+          return res.status(500).json({ message: error })
+        }
+        return res.status(200).json({ message: 'Deleted' })
+      }
+      default: {
+        res.setHeader('Allow', ['GET', 'DELETE', 'POST'])
+        return res.status(405).end(`Method ${method} Not Allowed`)
+      }
+    }
+  } else {
+    console.error('ERR - Unauthorized attempt at /api/tags')
+    return res.status(403).end('Unauthorized')
+  }
+}
