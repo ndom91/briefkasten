d83e4f181abc29e0760207051f2b22c09e40e04c
ndom91 <yo@ndo.dev>
2022-05-22 21:41:46 +0200

    feat: 404 and 500 pages

--- src/pages/500.js
+++ src/pages/500.js
@@ -7,7 +7,7 @@ export default function Settings() {
   return (
     <Layout>
       <Head>
-        <title>Briefkasten | 404</title>
+        <title>Briefkasten | 500</title>
       </Head>
       <section className="col-span-2 flex max-w-8xl flex-col items-center space-y-20 px-4 py-24">
         <Image
@@ -18,9 +18,9 @@ export default function Settings() {
           className="h-64 w-full rounded-lg object-cover"
         />
         <div className="mx-auto w-full text-center lg:w-2/3">
-          <h1 className="mb-4 text-6xl font-thin text-gray-900">404</h1>
+          <h1 className="mb-4 text-6xl font-thin text-gray-900">500</h1>
           <p className="mb-3 text-xl font-bold text-gray-900 md:text-2xl">
-            Oh no! We couldn’t find the page you were looking for.
+            Oh no! There seems to be an issue on our side.
           </p>
           <p className="mb-3 text-lg font-medium text-gray-700">
             Have questions? Head over to our{' '}
