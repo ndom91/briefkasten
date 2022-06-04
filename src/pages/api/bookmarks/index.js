baf01d6a8670c9403b7365b82fc2e6c37c0aee46
ndom91 <yo@ndo.dev>
2022-06-04 17:09:13 +0200

    feat: unauthorized POST

--- src/pages/api/bookmarks/index.js
+++ src/pages/api/bookmarks/index.js
@@ -18,183 +18,181 @@ export default async function handler(req, res) {
     ? `${protocol}://${headers.host}`
     : 'http://localhost:3001'
 
-  if (session) {
-    switch (method) {
-      case 'POST': {
-        const {
-          userId,
-          url,
-          title = '',
-          category = '',
-          desc = '',
-          tags = [],
-        } = body
-        if (!url) {
-          return res
-            .status(400)
-            .json({ message: 'Missing required field: url' })
-        }
-        let metadata = {
-          title: '',
-          image: '',
-          description: '',
-        }
+  switch (method) {
+    case 'POST': {
+      const {
+        userId,
+        url,
+        title = '',
+        category = '',
+        desc = '',
+        tags = [],
+      } = body
+      if (!url) {
+        return res.status(400).json({ message: 'Missing required field: url' })
+      }
+      let metadata = {
+        title: '',
+        image: '',
+        description: '',
+      }
 
-        // First fetch any additional metadata about the URL
-        const resp = await fetch(url)
-        metadata = await metascraper({ html: await resp.text(), url: url })
+      // First fetch any additional metadata about the URL
+      const resp = await fetch(url)
+      metadata = await metascraper({ html: await resp.text(), url: url })
 
-        if (!metadata.image) {
-          // Generate image with puppeteer if we didnt get one from metadata
-          const imageData = await fetch(
-            `${baseUrl}/api/bookmarks/image?url=${encodeURIComponent(url)}`
-          )
-          const imageBlob = await imageData.blob()
-          const dataUrl = await asyncFileReader(imageBlob)
+      if (!metadata.image) {
+        // Generate image with puppeteer if we didnt get one from metadata
+        const imageData = await fetch(
+          `${baseUrl}/api/bookmarks/image?url=${encodeURIComponent(url)}`
+        )
+        const imageBlob = await imageData.blob()
+        const dataUrl = await asyncFileReader(imageBlob)
 
-          const imagekit = new ImageKit({
-            publicKey: process.env.IMAGEKIT_PUB_KEY,
-            privateKey: process.env.IMAGEKIT_PRIV_KEY,
-            urlEndpoint: process.env.IMAGEKIT_URL,
-          })
-
-          // Upload image body to ImageKit
-          const imageRes = await imagekit.upload({
-            file: dataUrl,
-            fileName: `${new URL(url).hostname}.jpg`,
-          })
-
-          metadata.image = imageRes.url
-        }
-
-        // Begin inserting into db
-        // First, bookmark since we need its ID for later inserts
-        let upsertTagRes
-        const upsertBookmarkRes = await prisma.bookmark.upsert({
-          include: {
-            category: true,
-          },
-          create: {
-            url,
-            title: title.length ? title : metadata.title,
-            image: metadata.image,
-            desc: desc.length ? desc : metadata.description,
-            user: {
-              connect: {
-                id: userId,
-              },
-            },
-            category: category
-              ? {
-                  connect: {
-                    name_userId: {
-                      name: category,
-                      userId,
-                    },
-                  },
-                }
-              : {},
-          },
-          update: {
-            url,
-            title: title.length ? title : metadata.title,
-            image: metadata.image,
-            desc: desc.length ? desc : metadata.description,
-            category: category
-              ? {
-                  connect: {
-                    name_userId: {
-                      name: category,
-                      userId,
-                    },
-                  },
-                }
-              : {},
-          },
-          where: { url_userId: { url: url, userId: userId } },
+        const imagekit = new ImageKit({
+          publicKey: process.env.IMAGEKIT_PUB_KEY,
+          privateKey: process.env.IMAGEKIT_PRIV_KEY,
+          urlEndpoint: process.env.IMAGEKIT_URL,
         })
 
-        // Next, if there are tags, insert them sequentially
-        if (tags && tags.filter(Boolean).length > 0) {
-          upsertTagRes = await Promise.all(
-            tags.map(async (tag) => {
-              return await prisma.tag.upsert({
-                create: {
-                  name: tag,
-                  userId,
-                },
-                update: {
-                  name: tag,
-                },
-                where: {
+        // Upload image body to ImageKit
+        const imageRes = await imagekit.upload({
+          file: dataUrl,
+          fileName: `${new URL(url).hostname}.jpg`,
+        })
+
+        metadata.image = imageRes.url
+      }
+
+      // Begin inserting into db
+      // First, bookmark since we need its ID for later inserts
+      let upsertTagRes
+      const upsertBookmarkRes = await prisma.bookmark.upsert({
+        include: {
+          category: true,
+        },
+        create: {
+          url,
+          title: title.length ? title : metadata.title,
+          image: metadata.image,
+          desc: desc.length ? desc : metadata.description,
+          user: {
+            connect: {
+              id: userId,
+            },
+          },
+          category: category
+            ? {
+                connect: {
                   name_userId: {
-                    name: tag,
+                    name: category,
                     userId,
                   },
                 },
-              })
-            })
-          )
+              }
+            : {},
+        },
+        update: {
+          url,
+          title: title.length ? title : metadata.title,
+          image: metadata.image,
+          desc: desc.length ? desc : metadata.description,
+          category: category
+            ? {
+                connect: {
+                  name_userId: {
+                    name: category,
+                    userId,
+                  },
+                },
+              }
+            : {},
+        },
+        where: { url_userId: { url: url, userId: userId } },
+      })
 
-          // Finally, link the tags to bookmark in intermediate join table
-          await prisma.tagsOnBookmarks.createMany({
-            data: upsertTagRes.map((tag) => ({
-              bookmarkId: upsertBookmarkRes.id,
-              tagId: tag.id,
-            })),
-          })
-        }
-
-        res.setHeader('Access-Control-Allow-Origin', '*')
-        return res
-          .status(200)
-          .json({ data: { ...upsertBookmarkRes, tags: upsertTagRes ?? [] } })
-      }
-      case 'GET': {
-        let bookmarksResults
-        const { q, limit = 10 } = query
-        const { authorization: userId } = headers
-
-        if (!userId) {
-          return res.status(400).json({ message: 'Missing required field(s)' })
-        }
-
-        try {
-          bookmarksResults = await prisma.bookmark.findMany({
-            take: parseInt(limit),
-            distinct: ['url'],
-            where: {
-              AND: {
+      // Next, if there are tags, insert them sequentially
+      if (tags && tags.filter(Boolean).length > 0) {
+        upsertTagRes = await Promise.all(
+          tags.map(async (tag) => {
+            return await prisma.tag.upsert({
+              create: {
+                name: tag,
                 userId,
               },
-              OR: [
-                {
-                  desc: {
-                    search: `*${q}*`,
-                  },
+              update: {
+                name: tag,
+              },
+              where: {
+                name_userId: {
+                  name: tag,
+                  userId,
                 },
-                {
-                  url: {
-                    search: `*${q}*`,
-                  },
-                },
-                {
-                  title: {
-                    search: `*${q}*`,
-                  },
-                },
-              ],
-            },
+              },
+            })
           })
-        } catch (error) {
-          console.error('ERR', error)
-          return res.status(500).json({ message: error })
-        }
+        )
 
-        res.setHeader('Access-Control-Allow-Origin', '*')
-        return res.status(200).json({ results: bookmarksResults })
+        // Finally, link the tags to bookmark in intermediate join table
+        await prisma.tagsOnBookmarks.createMany({
+          data: upsertTagRes.map((tag) => ({
+            bookmarkId: upsertBookmarkRes.id,
+            tagId: tag.id,
+          })),
+        })
       }
-      case 'DELETE': {
+
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      return res
+        .status(200)
+        .json({ data: { ...upsertBookmarkRes, tags: upsertTagRes ?? [] } })
+    }
+    case 'GET': {
+      let bookmarksResults
+      const { q, limit = 10 } = query
+      const { authorization: userId } = headers
+
+      if (!userId) {
+        return res.status(400).json({ message: 'Missing required field(s)' })
+      }
+
+      try {
+        bookmarksResults = await prisma.bookmark.findMany({
+          take: parseInt(limit),
+          distinct: ['url'],
+          where: {
+            AND: {
+              userId,
+            },
+            OR: [
+              {
+                desc: {
+                  search: `*${q}*`,
+                },
+              },
+              {
+                url: {
+                  search: `*${q}*`,
+                },
+              },
+              {
+                title: {
+                  search: `*${q}*`,
+                },
+              },
+            ],
+          },
+        })
+      } catch (error) {
+        console.error('ERR', error)
+        return res.status(500).json({ message: error })
+      }
+
+      res.setHeader('Access-Control-Allow-Origin', '*')
+      return res.status(200).json({ results: bookmarksResults })
+    }
+    case 'DELETE': {
+      if (session) {
         const imagekit = new ImageKit({
           publicKey: process.env.IMAGEKIT_PUB_KEY,
           privateKey: process.env.IMAGEKIT_PRIV_KEY,
@@ -231,14 +229,14 @@ export default async function handler(req, res) {
           return res.status(500).json({ message: error })
         }
         return res.status(200).json({ message: 'Deleted' })
-      }
-      default: {
-        res.setHeader('Allow', ['GET', 'DELETE', 'POST'])
-        return res.status(405).end(`Method ${method} Not Allowed`)
+      } else {
+        console.error('ERR - Unauthorized attempt at /api/bookmarks')
+        return res.status(403).end('Unauthorized')
       }
     }
-  } else {
-    console.error('ERR - Unauthorized attempt at /api/bookmarks')
-    return res.status(403).end('Unauthorized')
+    default: {
+      res.setHeader('Allow', ['GET', 'DELETE', 'POST'])
+      return res.status(405).end(`Method ${method} Not Allowed`)
+    }
   }
 }
