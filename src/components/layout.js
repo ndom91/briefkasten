45fa459e59c40d704bc7a1cdb53c9cd850d351ae
ndom91 <yo@ndo.dev>
2022-06-11 14:57:03 +0200

    feat: next.js error pages

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,12 +1,11 @@
 import Meta from '@/components/meta'
 import Sidebar from '@/components/sidebar'
-// import Navigation from '@/components/navigation'
 
 export default function Layout({ children }) {
   return (
     <>
       <Meta />
-      <section className="mx-auto grid max-h-screen grid-cols-[minmax(150px,_15%)_1fr] grid-rows-[auto] gap-2 md:gap-4 overflow-hidden">
+      <section className="mx-auto grid h-full max-h-screen grid-cols-[minmax(150px,_15%)_1fr] grid-rows-1 gap-2 md:gap-4 overflow-hidden">
         <Sidebar />
         <main className="max-h-screen overflow-y-scroll p-4">{children}</main>
       </section>
