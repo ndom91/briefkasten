16f61c84e402a84f71ca377d25a2e678463e518d
ndom91 <yo@ndo.dev>
2022-06-24 00:37:01 +0200

    fix: 404 page

--- src/pages/404.jsx
+++ src/pages/404.jsx
@@ -20,16 +20,18 @@ export default function Settings() {
             </h1>
             <p className="mb-5 text-left text-base text-gray-800 md:text-xl">
               You might have the wrong address, or the page may have moved.
+              Please use the buttons below to go back to the homepage, or
+              contact us for support.
             </p>
             <div className="flex">
               <Link href="/">
-                <a className="mr-4 rounded-xl bg-slate-800 px-5 py-4 text-center text-base font-medium text-white transition duration-500 ease-in-out hover:bg-slate-900 hover:drop-shadow-lg focus:outline-none focus:ring-2 focus:ring-slate-800 focus:ring-offset-2 lg:px-10">
+                <a className="mr-4 rounded-lg bg-slate-800 px-5 py-2 text-center text-base font-medium text-white transition duration-500 ease-in-out hover:bg-slate-900 hover:drop-shadow-lg focus:outline-none focus:ring-2 focus:ring-slate-800 focus:ring-offset-2 lg:px-10">
                   Back to homepage
                 </a>
               </Link>
               <a
                 href="mailto:support-briefkasten@ndo.dev"
-                className="rounded-xl border-2 border-slate-800 px-5 py-4 text-center text-base font-medium text-slate-800 transition duration-500 ease-in-out hover:border-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-800 focus:ring-offset-2 lg:px-10"
+                className="rounded-lg border-2 border-slate-800 px-5 py-2 text-center text-base font-medium text-slate-800 transition duration-500 ease-in-out hover:border-slate-900 focus:outline-none focus:ring-2 focus:ring-slate-800 focus:ring-offset-2 lg:px-10"
               >
                 Contact us
               </a>
