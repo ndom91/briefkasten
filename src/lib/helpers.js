2c4378cdb9fc88b54e3504cc9cc47759fa29e083
ndom91 <yo@ndo.dev>
2022-06-25 23:29:52 +0200

    chore: moar cleanup

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -1,5 +1,3 @@
-import { Blob } from 'buffer'
-
 const prepareBase64DataUrl = (base64) =>
   base64
     .replace('data:image/jpeg;', 'data:image/jpeg;charset=utf-8;')
@@ -21,7 +19,6 @@ const asyncFileReader = async (blob) => {
   } else {
     let buffer = Buffer.from(await blob.text())
     return buffer.toString('base64')
-    // return 'data:' + blob.type + ';base64,' + buffer.toString('base64')
   }
 }
 
@@ -30,43 +27,4 @@ const range = (start, end) => {
   return Array.from({ length }, (_, i) => i + start)
 }
 
-const base64ToArrayBuffer = (base64) => {
-  // const binary_string = atob(base64)
-  const binary_string = Buffer.from(base64, 'base64').toString()
-  const len = binary_string.length
-  const bytes = new Uint8Array(len)
-  for (let i = 0; i < len; i++) {
-    bytes[i] = binary_string.charCodeAt(i)
-  }
-  return bytes.buffer
-}
-
-const base64ToBlob = (b64Data, contentType = '', sliceSize = 512) => {
-  console.log('b64toBlob', b64Data.substring(0, 30))
-  const byteCharacters = Buffer.from(b64Data, 'base64').toString()
-  // const byteCharacters = atob(b64Data)
-  const byteArrays = []
-
-  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
-    const slice = byteCharacters.slice(offset, offset + sliceSize)
-
-    const byteNumbers = new Array(slice.length)
-    for (let i = 0; i < slice.length; i++) {
-      byteNumbers[i] = slice.charCodeAt(i)
-    }
-
-    const byteArray = new Uint8Array(byteNumbers)
-    byteArrays.push(byteArray)
-  }
-
-  const blob = new Blob(byteArrays, { type: contentType })
-  return blob
-}
-
-export {
-  prepareBase64DataUrl,
-  base64ToArrayBuffer,
-  base64ToBlob,
-  asyncFileReader,
-  range,
-}
+export { prepareBase64DataUrl, asyncFileReader, range }
