ffaab1e0e2765413cbdff981866257c9a0fb7d4e
ndom91 <yo@ndo.dev>
2022-06-29 11:55:11 +0200

    feat: update imageProxy to edge runtime

--- src/pages/api/imageProxy.js
+++ src/pages/api/imageProxy.js
@@ -1,8 +1,27 @@
-const handler = async (req, res) => {
-  const url = decodeURIComponent(req.query.url)
-  const result = await fetch(url)
-  const body = await result.body
-  body.pipe(res)
+const handler = async (req) => {
+  const { searchParams } = new URL(req.url)
+  const url = searchParams.get('url')
+  if (url) {
+    const decodedUrl = decodeURIComponent(url)
+    const result = await fetch(decodedUrl)
+    const buffer = await result.blob()
+    return new Response(buffer, {
+      status: 200,
+      headers: {
+        'content-type': result.headers.get('content-type') ?? 'image/jpeg',
+      },
+    })
+  }
+  return new Response(JSON.stringify({ error: 'no URL provided' }), {
+    status: 400,
+    headers: {
+      'content-type': 'application/json',
+    },
+  })
 }
 
 export default handler
+
+export const config = {
+  runtime: 'experimental-edge',
+}
