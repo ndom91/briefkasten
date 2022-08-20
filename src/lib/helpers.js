16724969a4eb5c7b2c7c48b74cedd81babd5f59c
ndom91 <yo@ndo.dev>
2022-08-20 22:56:44 +0200

    fix: add desc property to serverTiming helper

--- src/lib/helpers.js
+++ src/lib/helpers.js
@@ -33,20 +33,24 @@ export const serverTiming = {
       },
     }
   },
-  measure: (name) => {
+  measure: (name, desc) => {
     const now = performance.now()
     if (serverTiming.timings[name]?.start) {
       serverTiming.timings[name].end = now
       serverTiming.timings[name].dur = now - serverTiming.timings[name].start
+      if (desc) serverTiming.timings[name].desc = desc
     } else {
       serverTiming.timings[name] = { start: now }
+      if (desc) serverTiming.timings[name].desc = desc
     }
   },
   setHeader: () => {
     serverTiming.measure('total')
     return Object.entries(serverTiming.timings)
       .map(([name, measurements]) => {
-        return `${name};dur=${measurements.dur}`
+        return `${name};${
+          measurements.desc ? `desc="${measurements.desc}";` : ''
+        }dur=${measurements.dur}`
       })
       .join(',')
   },
