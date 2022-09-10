c467b7ef11b047aa5ab71b137072371cd2e9c604
ndom91 <yo@ndo.dev>
2022-09-10 23:27:40 +0200

    fix: bump sentry tracesSampleRate to 1.0

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -10,6 +10,6 @@ Sentry.init({
   dsn:
     SENTRY_DSN ||
     'https://2b0f5bb15ddd4feeb9fbed676243f0b6@o1287700.ingest.sentry.io/6503207',
-  tracesSampleRate: 0.5,
-  allowUrls: ['localhost:3001', 'briefkastenhq.com'],
+  tracesSampleRate: 1.0,
+  allowUrls: [/localhost:3001/, /briefkastenhq\.com/i],
 })
