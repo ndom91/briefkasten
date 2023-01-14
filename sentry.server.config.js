50090694a867b4340431adce7ec38ef9da3477f5
ndom91 <yo@ndo.dev>
2023-01-14 19:52:31 +0100

    fix: rm hardcoded sentry dsn

--- sentry.server.config.js
+++ sentry.server.config.js
@@ -7,9 +7,7 @@ import * as Sentry from '@sentry/nextjs'
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
-  dsn:
-    SENTRY_DSN ||
-    'https://2b0f5bb15ddd4feeb9fbed676243f0b6@o1287700.ingest.sentry.io/6503207',
+  dsn: SENTRY_DSN,
   tracesSampleRate: 1.0,
   allowUrls: [/localhost:3001/, /briefkastenhq\.com/i],
 })
