bf9ebf55cd8bcf98a9854891939a7a5c7fdb0729
ndom91 <yo@ndo.dev>
2023-03-26 12:14:56 +0200

    fix: lower Sentry traces sampling rate on all platforms

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -8,6 +8,6 @@ const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
   dsn: SENTRY_DSN,
-  tracesSampleRate: 1.0,
-  allowUrls: [/localhost:3001/, /briefkastenhq\.com/i],
+  tracesSampleRate: 0.5,
+  allowUrls: [/briefkastenhq\.com/i],
 })
--- sentry.edge.config.js
+++ sentry.edge.config.js
@@ -3,7 +3,7 @@ import * as Sentry from '@sentry/nextjs'
 const SENTRY_DSN = process.env.SENTRY_DSN || process.env.NEXT_PUBLIC_SENTRY_DSN
 
 Sentry.init({
-  dsn: SENTRY_DSN || 'YOUR DSN HERE',
-  tracesSampleRate: 1.0,
-  allowUrls: [/localhost:3001/, /briefkastenhq\.com/i],
+  dsn: SENTRY_DSN,
+  tracesSampleRate: 0.5,
+  allowUrls: [/briefkastenhq\.com/i],
 })
