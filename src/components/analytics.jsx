69a5b485d3c1d633c5697923e6d9d573f5198bd7
ndom91 <yo@ndo.dev>
2024-07-11 13:28:32 +0200

    fix: useEffect swetrix

--- src/components/analytics.jsx
+++ src/components/analytics.jsx
@@ -1,19 +1,21 @@
 'use client'
 
 import * as Swetrix from 'swetrix'
-import { Suspense } from 'react'
+import { useEffect, Suspense } from 'react'
 import { usePathname } from 'next/navigation'
 
 function SwetrixComponent() {
   const pathname = usePathname()
 
-  if (window.location.host === 'briefkastenhq.com') {
-    Swetrix.init(process.env.NEXT_PUBLIC_SWETRIX_PROJECT, {
-      apiURL: process.env.NEXT_PUBLIC_SWETRIX_API_HOST,
-    })
-    let url = pathname
-    Swetrix.trackPageview(url)
-  }
+  useEffect(() => {
+    if (window.location.host === 'briefkastenhq.com') {
+      Swetrix.init(process.env.NEXT_PUBLIC_SWETRIX_PROJECT, {
+        apiURL: process.env.NEXT_PUBLIC_SWETRIX_API_HOST,
+      })
+      let url = pathname
+      Swetrix.trackPageview(url)
+    }
+  }, [pathname])
   return <div></div>
 }
 
